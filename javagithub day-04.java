32bd5d5546319fe0ab7dd50d765ec12b656fc66c
serusharma <sharmasachin80505@gmail.com>
2024-10-02 22:26:04 +0530

    Add files via upload

---
+++ javagithub day-04.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+	    int a = 5;
+	    int b = 1;
+	    int diff = a - b;
+	    
+	    System.out.printf(diff);
+	    
+	
+	}
+}
